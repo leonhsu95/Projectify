dcd67c8497eb1b1fe2b692def6f6e9d4bf77ee5a
Leon Hsu <leonhsu95@gmail.com>
2021-06-08 19:12:58 +1000

    New Database Seed for Campaign

--- seeds/campaign.js
+++ seeds/campaign.js
@@ -9,6 +9,20 @@ const campaignData =
     "ig_clicks": 2500,
     "projectID": 1,
   },
+  {
+    "unique_visitors": 1500,
+    "total_visitors": 2100,
+    "fb_clicks": 3000,
+    "ig_clicks": 2700,
+    "projectID": 1,
+  },
+  {
+    "unique_visitors": 2200,
+    "total_visitors": 2500,
+    "fb_clicks": 3200,
+    "ig_clicks": 2300,
+    "projectID": 1,
+  },
 ]
 
 const seedCampaigns = () => Campaign.bulkCreate(campaignData);
