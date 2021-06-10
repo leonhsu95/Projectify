129daf789d6a898a21ce31126ad0ff1d2d75ad82
Leon Hsu <leonhsu95@gmail.com>
2021-06-10 19:22:37 +1000

    Started api/item-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const projectRoutes = require('./project-routes');
+const itemRoutes = require('./item-routes');
 
 router.use('/users', userRoutes);
+router.use('/items', itemRoutes);
 router.use('/projects', projectRoutes);
 
 module.exports = router;
