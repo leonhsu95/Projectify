3207c1a1b0c4afb2388519197b10a267196f4c28
Leon Hsu <leonhsu95@gmail.com>
2021-06-14 17:39:10 +1000

    Added Icon, Model Adjustment and Min-Height changed

--- models/User.js
+++ models/User.js
@@ -57,9 +57,9 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [8],
-      },
+      // validate: {
+      //   len: [8],
+      // },
     },
   },
   {
